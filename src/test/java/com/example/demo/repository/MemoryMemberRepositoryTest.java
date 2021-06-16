945ebd6094034de70755f465cfbf378540f99e96
최유미 <78770263+youmichoi93@users.noreply.github.com>
2021-06-16 19:03:27 +0900

    Add files via upload

---
+++ src/test/java/com/example/demo/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,76 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Member;
+import org.hamcrest.collection.IsCollectionWithSize;
+import org.hamcrest.object.IsCompatibleType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.ServiceConfigurationError;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.OPTIONAL;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+
+
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+
+    }
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member member3 = new Member();
+        member3.setName("spring3");
+        repository.save(member3);
+
+        Member result = repository.findByName("spring3").get();
+        assertThat(result).isEqualTo(member3);
+    }
+
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member member3 = new Member();
+        member3.setName("spring3");
+        repository.save(member3);
+
+        List<Member> result = repository.findAll();
+        assertThat(result.size()).isEqualTo(3);
+
+
+    }
+
+
+}//
+
